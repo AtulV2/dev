a7509593efce3d866ac755b941323c306932b00e
AtulV2 <153658280+AtulV2@users.noreply.github.com>
2025-10-13 19:09:31 +0530

    Que 1 is working

--- MCA/Atul_Verma/Python/p2q1.py
+++ MCA/Atul_Verma/Python/p2q1.py
@@ -1,55 +1,28 @@
 # Write a Python program to reverse only the vowels in a given string, keeping other characters in their original positions.
 
-# input_str = input("Enter a string: ")
-input_str = 'a1e2i3o4u5a6'
+input_str = input("Enter a string: ") # eg: 'a1e2i3o4u5a6'
 
 list_str = list(input_str)
-
 vowels = 'aeiouAEIOU'
-
-vowel_list = []
+v_list = []
 
 for char in list_str:
     if char in vowels:
-        vowel_list.append(char)
+        v_list.append(char)
     
-
 # Reverse the vowel list
-vowel_list = vowel_list[::-1]
+v_list_rev = v_list[::-1]
 
 result = list_str
-for i in range(len(list_str) -1):
-    
-    if result[i] in vowels:
-        result[i] = vowel_list[(len(vowel_list) - 1) - i]
+vowel_index = 0
+for i in range(len(list_str)):
+    if list_str[i] in vowels:
+        result[i] = v_list_rev[vowel_index]
+        vowel_index += 1
+    else:
+        result[i] = list_str[i]
 
-        
-print("String with reversed vowels:", result)
+print("String with reversed vowels:", ''.join(result)) 
+# String with reversed vowels: a1u2o3i4e5a6
 
 
-
-
-# input_str = 'a1e2i3o4u5a6'
-
-# list_str = list(input_str)
-
-# vowels = 'aeiouAEIOU'
-
-# vowel_list = []
-
-# # Collect vowels
-# for char in list_str:
-#     if char in vowels:
-#         vowel_list.append(char)
-
-# # Reverse the vowel list
-# vowel_list = vowel_list[::-1]
-
-# # Replace vowels in original positions
-# vowel_index = 0
-# for i in range(len(list_str)):
-#     if list_str[i] in vowels:
-#         list_str[i] = vowel_list[vowel_index]
-#         vowel_index += 1
-
-# print("String with reversed vowels:", ''.join(list_str))
